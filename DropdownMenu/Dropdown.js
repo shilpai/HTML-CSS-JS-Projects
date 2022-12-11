e579026f55a0ed0cb16079270418483d4ca60398
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2022-12-11 21:51:20 +0530

    initial commit 🚀

---
+++ DropdownMenu/Dropdown.js
@@ -0,0 +1,20 @@
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("arrow");
+
+
+const toggleDropdown = function () {
+    dropdownMenu.classList.toggle("show");
+    toggleArrow.classList.toggle("arrow");
+  };
+
+  dropdownBtn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    toggleDropdown();
+  });
+
+  document.documentElement.addEventListener("click", function () {
+    if (dropdownMenu.classList.contains("show")) {
+      toggleDropdown();
+    }
+  });
